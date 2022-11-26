ff7fdfeaa7aac2aedb4c2a9f10e890e129efdbf2
meena <annmm537@gmail.com>
2022-11-26 14:56:08 -0800

    addAttributes

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -14,8 +14,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -63,6 +65,17 @@ public class Person {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
+
+    @Column(nullable = true)
+    private Integer height=0;
+
+    @Column(nullable = true)
+    private Double weight=0.0;
+
+    @Column(nullable = true)
+    private Integer goalSteps =0;
+    //private double weight;
+    //private int goalSteps;
     
 
     /* HashMap is used to store JSON for daily "stats"
@@ -79,12 +92,16 @@ public class Person {
     
 
     // Constructor used when building object from an API
-    public Person(String email, String password, String name , Date dob ) {
+    public Person(String email, String password, String name , Date dob , int height , double weight, int goalSteps) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.dob = dob;
+        this.height = height;
+        this.weight = weight;
     }
+
+    
 
     // A custom getter to return age from dob attribute
     public int getAge() {
@@ -94,34 +111,50 @@ public class Person {
         return -1;
     }
 
+    public int getGoalSteps() {
+        if (this.getAge() >= 20) {
+            return 10000; } 
+        else if ((this.getAge() < 20) && (this.getAge() > 11)) {
+            return 12500;
+        }
+        return 13500;
+    }
+
+
     public String personNameToString(){
         return ( "{ \"name\": "  +this.name + " }" );
      }	
 
-    public String personEmailToString(){
-        return ( "{ \"name\": "  +this.name+  ", " + "\"dayOfYear\": "  +this.email+ " }"  );
-     }	
-
-    public String toString() { 
-        return personNameToString(); 
+    public String personAttributesToString(){
+        return ( "{ \"name\": "  +this.name+  ", " + "\"email\": "  +this.email+ ", " + "\"password\": "  +this.password+ ", " + "\"dob\": "  +this.dob+ ", " + "\"age\": "  +this.getAge()+", " + "\"height\": "  +this.height+", " + "\"weight\": "  +this.weight+ "\"goalSteps\": "  +this.getGoalSteps()+" }"  );
      }
+
+    // public String toString() { 
+    //     return personNameToString(); 
+    //  }
 
     //Tester Method for Person POJO
     public static void main(String[] args) {
         Person testPersonNotBob = new Person();
         System.out.println(testPersonNotBob);
         System.out.println("zero argument get email: " + testPersonNotBob.getEmail());
-        LocalDate birthday = LocalDate.of(1995, 1, 1);
-        Date date = Date.from(birthday.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Person testPerson = new Person("bob@gmail.com", "hammer10!", "Bob the Builder", date);
+
+        LocalDate locBirthday = LocalDate.of(2011, 1, 1);
+        Date birthday = Date.from(locBirthday.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        Person testPerson = new Person("bob@gmail.com", "hammer10!", "Bob the Builder", birthday, 65, 150.2, 10000);
         System.out.println("email: " + testPerson.getEmail());
         System.out.println("name: " + testPerson.getName());
         System.out.println("password: " + testPerson.getPassword());
-        testPerson.setPassword("theNewBlob");
+
+        testPerson.setPassword("theNewHammer11!");
         //testPerson.setDob(2022-11-10);
         System.out.println("new password: " + testPerson.getPassword());
         System.out.println("date of birth: " + testPerson.getDob());
         System.out.println("age: " + testPerson.getAge());
+
+        System.out.println(testPerson.personNameToString());
+        System.out.println(testPerson.personAttributesToString());
     }
 
 
--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -65,7 +65,10 @@ public class PersonApiController {
     public ResponseEntity<Object> postPerson(@RequestParam("email") String email,
                                              @RequestParam("password") String password,
                                              @RequestParam("name") String name,
-                                             @RequestParam("dob") String dobString) {
+                                             @RequestParam("dob") String dobString,
+                                             @RequestParam("height") int height,
+                                             @RequestParam("weight") double weight,
+                                             @RequestParam("goalSteps") int goalSteps) {
         Date dob;
         try {
             dob = new SimpleDateFormat("MM-dd-yyyy").parse(dobString);
@@ -73,7 +76,7 @@ public class PersonApiController {
             return new ResponseEntity<>(dobString +" error; try MM-dd-yyyy", HttpStatus.BAD_REQUEST);
         }
         // A person object WITHOUT ID will create a new record with default roles as student
-        Person person = new Person(email, password, name, dob);
+        Person person = new Person(email, password, name, dob , height , weight, goalSteps);
         repository.save(person);
         return new ResponseEntity<>(email +" is created successfully", HttpStatus.CREATED);
     }
